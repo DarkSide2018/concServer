65e3119bdecf78aca3965b0f347c9382f531aca8
DefNeoRoman <d89056411060@yandex.ru>
2019-01-02 15:09:26 +0300

    Initial commit

---
+++ src/main/java/com/concurrent/concurrent/controller/CarController.java
@@ -0,0 +1,34 @@
+package com.concurrent.concurrent.controller;
+
+import com.concurrent.concurrent.model.Car;
+import com.concurrent.concurrent.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RestController
+public class CarController {
+    private CarRepository repository;
+
+    @Autowired
+    public CarController(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/cool-cars")
+    public Collection<Car> coolCars() {
+        return repository.findAll().stream()
+                .filter(this::isCool)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isCool(Car car) {
+        return !car.getName().equals("AMC Gremlin") &&
+                !car.getName().equals("Triumph Stag") &&
+                !car.getName().equals("Ford Pinto") &&
+                !car.getName().equals("Yugo GV");
+    }
+}
---
+++ src/main/java/com/concurrent/concurrent/service/CarService.java
@@ -0,0 +1,8 @@
+package com.concurrent.concurrent.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarService {
+
+}
