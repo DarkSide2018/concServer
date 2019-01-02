f7a58f22a2378091b14e077778d1a4ee7dac2dda
DefNeoRoman <d89056411060@yandex.ru>
2019-01-02 16:26:36 +0300

    only four classes

--- src/main/java/com/concurrent/concurrent/controller/CarController.java
+++ src/main/java/com/concurrent/concurrent/controller/CarController.java
@@ -3,6 +3,7 @@ package com.concurrent.concurrent.controller;
 import com.concurrent.concurrent.model.Car;
 import com.concurrent.concurrent.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ public class CarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
